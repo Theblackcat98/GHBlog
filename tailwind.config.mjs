6ffbdf06cf2b09f5c6592ec89ebe366cd96a55b3
theblackcat98 <ntantardini@gmail.com>
2024-03-27 16:32:43 -0700

    Added Nextui and React

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()]
+}
